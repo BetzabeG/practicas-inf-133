0019d118612438c3649965fcd3fee2434f02a02c
Betzbe Gutierrez <betzabegutierrezmorales@gmail.com>
2024-03-31 16:42:36 -0400

    ejercicios

--- practica-uno/ejercicio-uno/soap_client.py
+++ practica-uno/ejercicio-uno/soap_client.py
@@ -1,1 +1,19 @@
-print("Pronto iniciamos...")
+from zeep import Client
+
+client = Client('http://localhost:8000/')
+
+result_Suma = client.service.SumaDosNumeros(num1=12, num2=6)
+print("Operacion de la suma de dos numeros enteros")
+print(f"La suma de 12 y 6 es: {result_Suma}")
+
+result_Resta = client.service.RestaDosNumeros(num1=24, num2=7)
+print("\nOperacion de la resta de dos numeros enteros")
+print(f"La resta de 24 - 7 es: {result_Resta}")
+
+result_Multiplicacion = client.service.MultiplicacionDosNumeros(num1=12, num2=4)
+print("\nOperacion de la multiplicacion de dos numeros enteros")
+print(f"La multiplicacion de 12 * 4 es: {result_Multiplicacion}")
+
+result_Division = client.service.DivisionDosNumeros(num1=48, num2=4)
+print("\nOperacion de la divicion de dos numeros enteros")
+print(f"La division de 48 entre 4 es: {result_Division}")
--- practica-uno/ejercicio-uno/soap_server.py
+++ practica-uno/ejercicio-uno/soap_server.py
@@ -2,8 +2,20 @@ from http.server import HTTPServer
 from pysimplesoap.server import SoapDispatcher, SOAPHandler
 
 # Realizamos la funcion para la operacion de la suma
-def suma(num1, num2):
+def sumaDosNumeros(num1, num2):
     return num1 + num2
+
+# Realizamos la funcion para la operacion de la resta entre dos numeros enteros
+def restaDosNumeros(num1, num2):
+    return num1 - num2
+
+# Realizamos la funcion para la operacion de la multiplicacion de dos numeros enteros
+def multiplicacionDosNumeros(num1, num2):
+    return num1 * num2
+
+# Realizamos la funcion para la operacion de la division de dos numeros enteros
+def divisionDosNumeros(num1, num2):
+    return num1 // num2
 
 # Creamos la ruta del servidor SOAP
 dispatcher = SoapDispatcher(
@@ -17,10 +29,33 @@ dispatcher = SoapDispatcher(
 
 # Registramos la operacion de la suma
 dispatcher.register_function(
-    "Suma",
-    suma,
-    returns = {"resultado: ": int},
-    args = {"num1": int, "num2":int}
+    "SumaDosNumeros",
+    sumaDosNumeros,
+    returns = {"resultado": int},
+    args = {"num1": int, "num2": int}
+)
+
+# Registramos la operacion de la resta
+dispatcher.register_function(
+    "RestaDosNumeros",
+    restaDosNumeros,
+    returns = {"resultado": int},
+    args = {"num1": int, "num2": int}
+)
+# Registramos la operacion de la multiplicacion
+dispatcher.register_function(
+    "MultiplicacionDosNumeros",
+    multiplicacionDosNumeros,
+    returns = {"resultado": int},
+    args = {"num1": int, "num2": int}
+)
+
+# Registramos la operacion de la division
+dispatcher.register_function(
+    "DivisionDosNumeros",
+    divisionDosNumeros,
+    returns = {"resultado": int},
+    args = {"num1": int, "num2": int}
 )
 
 # Iniciamos el servidor HTTP
