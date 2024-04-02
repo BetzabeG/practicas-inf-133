cf53b6a8137ca0a9c032f0c2a461bc831d606924
Betzbe Gutierrez <betzabegutierrezmorales@gmail.com>
2024-04-02 10:54:43 -0400

    empezando con la solucion ejercicio 3

---
+++ practica-uno/ejercicio-tres/rest_server.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+estudiantes = [
+    {
+        "ci": 1,
+        "nombre": "Fernando",
+        "apellido": "Garcia",
+        "edad": 25,
+        "genero": "Masculino",
+        "diagnostico": "Critico",
+        "doctor": "Jorge",
+    },
+]
